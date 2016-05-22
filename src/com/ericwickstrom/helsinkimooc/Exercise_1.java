627e0690bf6da7c2415f1c33c6baf800f3e1ed88
Eric Wickstrom <wickstrome@gmail.com>
2016-05-22 10:51:24 -0500

    initial commit and also ex1, ex2, ex3, ex4

---
+++ src/com/ericwickstrom/helsinkimooc/Exercise_1.java
@@ -0,0 +1,15 @@
+/* 
+ * Exercise 1
+ * Create a program that prints your name to the screen.
+ * The program output should resemble the following:
+ * Jane Doe
+ */
+
+package com.ericwickstrom.helsinkimooc;
+
+public class Exercise_1 {
+	public static void main(String[] args){
+		System.out.println("Eric Wickstrom");
+	}
+
+}
---
+++ src/com/ericwickstrom/helsinkimooc/Exercise_3.java
@@ -0,0 +1,26 @@
+/*
+ * Exercise 3
+ * Create a program that prints the following:
+ *     *
+ *    ***
+ *   *****
+ *  *******
+ * *********
+ *     *
+ */
+
+package com.ericwickstrom.helsinkimooc;
+
+public class Exercise_3 {
+	public static void main(String[] args){
+
+		String art = 
+		"		    *"+"\n"+
+		"		   ***"+"\n"+
+		"		  *****"+"\n"+
+		"		 *******"+"\n"+
+		"		*********"+"\n"+
+		"		    *";
+		System.out.print(art);
+	}
+}
